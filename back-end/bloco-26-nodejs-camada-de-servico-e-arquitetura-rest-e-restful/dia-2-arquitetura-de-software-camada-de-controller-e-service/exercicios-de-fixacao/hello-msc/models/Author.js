5d555746babbc3ae8e92285a3c00d86b9d2041f6
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-02 16:43:20 -0300

    add camadas de service e controller

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-de-fixacao/hello-msc/models/Author.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios-de-fixacao/hello-msc/models/Author.js
@@ -74,9 +74,27 @@ const create = async (firstName, middleName, lastName) =>
     .then((db) => db.collection('authors').insertOne({ firstName, middleName, lastName }))
     .then(result => getNewAuthor({ id: result.insertedId, firstName, middleName, lastName }));
 
+const findByName = async (firstName, middleName, lastName) => {
+  // determinamos se devemos buscar com ou sem o nome do meio
+  const query = middleName
+    ? { firstName, moddleName, lastName }
+    : { firstName, lastName };
+
+  // Executamos a consulta e retornamos o resultado
+  const db = await connection();
+  const author = await db.collection('authors').findOne(query);
+
+  // Caso nenhum author seja encontrado, devolvemos null
+  if (!author) return null;
+
+  // Caso contrário, retornamos o author encontrado
+  return getNewAuthor(author);
+};
+
 module.exports = {
   getAll,
   findById,
   isValid,
   create,
+  findByName,
 };
