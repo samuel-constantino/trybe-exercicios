fc307de8ad2517939eebdfc812537704abc2c4cf
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-11 15:38:41 -0300

    add .env file

--- back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cep-lookup/src/index.js
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exercicios/cep-lookup/src/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { cepRouter } = require('./routes');
 const { error } = require('./middlewares');
 
+require('dotenv').config();
+
 const app = express();
 
 app.get('/', (_req, res, _next) => {
@@ -13,6 +15,6 @@ app.use('/cep', cepRouter);
 
 app.use(error);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
