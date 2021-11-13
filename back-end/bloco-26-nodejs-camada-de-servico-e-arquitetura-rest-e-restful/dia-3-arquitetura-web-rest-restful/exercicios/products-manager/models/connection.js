3bff4032f4111c91cc8fa64b542045a0f72d937a
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-13 15:41:25 -0300

    configure setup

---
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-restful/exercicios/products-manager/models/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: 'localhost', 
+  user: 'tryber',
+  password: '12345678',
+  database: 'rest_exercicios'});
+
+module.exports = connection;
