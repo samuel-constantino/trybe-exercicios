7acf156c75212e47d04839b018ce1ee74bab9281
Samuel Constantino <samuelicapuidados@gmail.com>
2021-11-17 14:54:11 -0300

    add connection

---
+++ back-end/bloco-26-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-4-arquitetura-de-software-testando-as-camadas/exercicios-fixacao/api-movies/models/connection.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+
+let db = null;
+
+const connection = async () => {
+    if (db) return Promise.resolve(db);
+
+    const conn = await MongoClient.connect(MONGO_DB_URL, OPTIONS);
+
+    db = conn.db('model_example');
+
+    return db;
+};
+
+module.exports = connection;
