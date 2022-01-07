b321a108cb0ce0283dbc86f5f6cc4143b9dd5048
Samuel Constantino <samuelicapuidados@gmail.com>
2022-01-07 20:16:34 -0300

    add crud completo

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-daddos/exercicios/livraria/routers/bookRouter.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-2-orm-interface-da-aplicacao-com-o-banco-de-daddos/exercicios/livraria/routers/bookRouter.js
@@ -6,4 +6,12 @@ const route = express.Router();
 
 route.get('/', bookController.findAll);
 
+route.get('/:id', bookController.findByPk);
+
+route.post('/', bookController.create);
+
+route.put('/:id', bookController.update);
+
+route.delete('/:id', bookController.remove);
+
 module.exports = route;
