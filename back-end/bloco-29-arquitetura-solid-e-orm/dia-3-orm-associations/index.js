5f47ed9608f0c537bde3e3d060deff838c02fc0d
Samuel Constantino <samuelicapuidados@gmail.com>
2022-01-08 19:48:30 -0300

    implementa Eager Loading

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-3-orm-associations/index.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-3-orm-associations/index.js
@@ -17,6 +17,31 @@ app.get('/employees', async (_req, res) => {
   };
 });
 
+app.get('/employees/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const employee = await Employee.findOne({
+      where: { id },
+      include: [{
+        model: Address,
+        as: 'addresses',
+        attributes: {
+          exclude: ['number', 'employeeId', 'employee_id'],
+        }
+      }],
+    });
+
+    if (!employee) 
+      return res.status(404).json({ message: 'Funcionário não encontrado' });
+
+    return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  };
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
 
