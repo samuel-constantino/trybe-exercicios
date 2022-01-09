e291d578cfdb0f56c42a06895d4d301195f0fe1e
Samuel Constantino <samuelicapuidados@gmail.com>
2022-01-08 22:00:38 -0300

    implementa transação não gerenciada

--- back-end/bloco-29-arquitetura-solid-e-orm/dia-3-orm-associations/funcionarios-enderecos/index.js
+++ back-end/bloco-29-arquitetura-solid-e-orm/dia-3-orm-associations/funcionarios-enderecos/index.js
@@ -1,8 +1,15 @@
-// index.js
 const express = require('express');
+const { json } = require('body-parser');
+const Sequelize = require('sequelize');
+
 const { Address, Employee } = require('./models');
+const { development } = require('./config/config');
 
 const app = express();
+
+app.use(json());
+
+const sequelize = new Sequelize(development);
 
 app.get('/employees', async (_req, res) => {
   try {
@@ -49,6 +56,43 @@ app.get('/employees/:id', async (req, res) => {
     console.log(e.message);
     res.status(500).json({ message: 'Ocorreu um erro' });
   };
+});
+
+app.post('/employees', async (req, res) => {
+  // Primeiro iniciamos a transação
+  const t = await sequelize.transaction();
+
+  try {
+    const {
+      firstName,
+      lastName,
+      age,
+      city,
+      street,
+      number
+    } = req.body;
+
+    // Depois executamos as operações
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou até essa linha, quer dizer que nenhum erro ocorreu.
+    // Com isso, podemos finalizar a transação usando a função `commit`.
+    await t.commit();
+
+    return res.status(201).json({ message: 'Cadastrado com sucesso' });
+  }catch(e) {
+    await e.rollback();
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
