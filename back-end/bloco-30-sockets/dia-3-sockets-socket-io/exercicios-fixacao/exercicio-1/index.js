122431dd240bb0c710da72ee3793e2ec2b9a664c
Samuel Constantino <samuelicapuidados@gmail.com>
2022-01-18 15:16:16 -0300

    refatoracao

--- back-end/bloco-30-sockets/dia-3-sockets-socket-io/exercicios-fixacao/exercicio-1/index.js
+++ back-end/bloco-30-sockets/dia-3-sockets-socket-io/exercicios-fixacao/exercicio-1/index.js
@@ -1,32 +1,22 @@
 const express = require('express');
-
 const app = express();
-
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
-    cors: {
-        origin: 'http://localhost:3000', // url aceita pelo cors
-        methods: ['GET', 'POST'], // Métodos aceitos pela url
-    }
+  cors: {
+    origin: 'http://localhost:3000', // url aceita pelo cors
+    methods: ['GET', 'POST'], // Métodos aceitos pela url
+  },
 });
 
-io.on('connection', (socket) => {
-    console.log(`Usuário conectado. ID: ${socket.id}`);
-    
-    socket.emit('ola', 'Que bom que você chegou aqui!');
+app.use(express.static(__dirname + '/public'));
 
-    socket.on('ping', () => {
-        console.log(`Usuário ${socket.id} emitiu um ping!`);
+require('./sockets/ping')(io);
 
-        io.emit('pong', `${socket.id} enviou um ping!`); // Essa linha avisa ao cliente que o ping foi recebido
-    });
-});
-
-app.get('/', (_req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, () => {
-    console.log('Servidor ouvindo na porta 3000');
+  console.log('Servidor ouvindo na porta 3000');
 });
